df32220ff6f3c45af05541d7038f3b2cd31459e5
DarthChungo <aharomar2004@gmail.com>
2021-04-05 00:50:44 +0200

    Bugfix

--- src/output/TypingOutput.js
+++ src/output/TypingOutput.js
@@ -6,8 +6,12 @@ const TypingOutput = ({ content }) => (
   <TypingContainer sequence={content.content} typeSpeed={content.options.typeSpeed} deleteSpeed={content.options.deleteSpeed}></TypingContainer>
 );
 
-TypingOutput.propTypes = {
+TypingOutput.propTypes.content = {
   content: PropTypes.array.isRequired,
+  
+};
+
+TypingOutput.propTypes.content.options = {
   typeSpeed: PropTypes.number.isRequired,
   deleteSpeed: PropTypes.number.isRequired
 };
