512d89e4681fb7f22a3adb7213f70069be94de2c
DarthChungo <aharomar2004@gmail.com>
2021-04-05 00:31:23 +0200

    More options for typing output

--- src/output/TypingOutput.js
+++ src/output/TypingOutput.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TypingContainer from 'output/TypingContainer';
 
-const TypingOutput = ({ content }) => (
-  <TypingContainer sequence={content} deleteSpeed={0}></TypingContainer>
+const TypingOutput = ({ content, typeSpeed, deleteSpeed }) => (
+  <TypingContainer sequence={content} typeSpeed={typeSpeed} deleteSpeed={deleteSpeed}></TypingContainer>
 );
 
 TypingOutput.propTypes = {
-  content: PropTypes.array.isRequired
+  content: PropTypes.array.isRequired,
+  typeSpeed: PropTypes.number.isRequired,
+  deleteSpeed: PropTypes.number.isRequired
 };
 
 export default TypingOutput;
